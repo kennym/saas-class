065e42ec9e610beba531c46b5aa7eac99613146c
Kenny Meyer <knny.myer@gmail.com>
2012-10-12 14:08:07 -0300

    Change ID

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def sortable(title, column)
     th_class = sort_column == column ? "hilite" : nil
-    css_class = column + "_header"
-    content_tag(:th, link_to(title, {:sort_by => column}, {:class => css_class}), :class=> th_class)
+    link_id = column + "_header"
+    content_tag(:th, link_to(title, {:sort_by => column}, {:id => link_id}), :class=> th_class)
   end
 end

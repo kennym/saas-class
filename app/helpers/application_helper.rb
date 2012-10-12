b30ba3b42615c92fe21e5a70ae1f19f05730b72a
Kenny Meyer <knny.myer@gmail.com>
2012-10-12 13:31:37 -0300

    Sortable table by "Movie Title" and "Release date"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def sortable(title, column)
+    th_class = sort_column == column ? "hilite" : nil
+    css_class = column + "_header"
+    content_tag(:th, link_to(title, {:sort_by => column}, {:class => css_class}), :class=> th_class)
+  end
 end
